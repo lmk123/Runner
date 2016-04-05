51fc45f91c59f8a95221fce1d0db77f890f1ce61
lmk123 <me@limingkai.cn>
2016-04-05 12:15:59 +0800

    删除无用函数

--- src/vuex/getters.js
+++ src/vuex/getters.js
@@ -1,17 +1,3 @@
-/**
- * 获取用户当前训练状态
- * @param state
- * @returns {{week: number, no: number, finished: boolean, date: number}}
- */
-export function currentWork( state ) {
-  return {
-    week: state.week,
-    no: state.no,
-    finished: state.finished,
-    date: state.date
-  };
-}
-
 export function week( state ) {
   return state.week;
 }
