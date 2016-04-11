ad6f0d11fedc1b24e13d7496549f6ad99a1a4a1a
Milk Lee <me@limingkai.cn>
2016-04-11 23:20:35 +0800

    修复一个 bug

--- src/pages/running-choose.vue
+++ src/pages/running-choose.vue
@@ -2,7 +2,7 @@
   <f7-content-block title="选择适合你的训练"></f7-content-block>
   <div class="list-block media-list">
     <ul>
-      <li v-for="train in $parent.trains">
+      <li v-for="train in running.trains">
         <a class="item-link item-content" v-touch:tap="choose(train)">
           <div class="item-inner">
             <div class="item-title-row">
@@ -20,15 +20,21 @@
 </template>
 
 <script type="text/babel">
+  import { running } from '../vuex/getters';
   export default {
+    vuex: {
+      getters: {
+        running
+      }
+    },
     route: {
       activate() {
-        this.$parent.title = '选择训练项目';
+        this.running.title = '选择训练项目';
       }
     },
     methods: {
       choose( train ) {
-        this.$parent.train = train;
+        this.running.train = train;
         this.$route.router.go( '/running/start' );
       }
     }
